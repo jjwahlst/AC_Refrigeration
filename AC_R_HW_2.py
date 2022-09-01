fe8b27d42eee5124828fd39925de5f0f904b0a59
jjwahlst <jjwahlst@asu.edu>
2022-09-01 15:05:25 -0700

    Figure out how to install, import, and use CoolProp functions.

---
+++ AC_R_HW_2.py
@@ -0,0 +1,6 @@
+# This is the file for homework #2
+
+# figure out how to import CoolProp fucntions
+from CoolProp.CoolProp import PropsSI
+# quick check example from documentation
+print(PropsSI('D', 'T', 298.15, 'P', 100e5, 'CO2'))
