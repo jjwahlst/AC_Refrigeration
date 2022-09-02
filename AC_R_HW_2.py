7bf8ea30c7f0053dff9c417d9a53370123fa379e
jjwahlst <jjwahlst@asu.edu>
2022-09-02 11:47:34 -0700

    Updating file

--- AC_R_HW_2.py
+++ AC_R_HW_2.py
@@ -3,11 +3,4 @@
 # figure out how to import CoolProp fucntions
 from CoolProp.CoolProp import PropsSI
 # quick check example from documentation
-# print(PropsSI('D', 'T', 298.15, 'P', 100e5, 'CO2'))
-
-h1 = PropsSI('H','T',273.15,'Q',1,'R134A')
-s1 = PropsSI('S','T',273.15,'Q',1,'R134A')
-Psat = PropsSI('P','T',(333.15),'Q',1,'R134A')
-print(h1)
-h2 = PropsSI('H','S', s1, 'P', Psat,'R134A')
-print(h2)
+print(PropsSI('D', 'T', 298.15, 'P', 100e5, 'CO2'))
