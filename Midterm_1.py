f73a95004f2ebee520ad77587c5fb5a8a16bb650
jjwahlst <jjwahlst@asu.edu>
2022-09-28 13:24:19 -0700

    Set up Cool Prop for today's Midterm.

---
+++ Midterm_1.py
@@ -0,0 +1,12 @@
+# Code for Midterm 1
+
+# Import Cool Prop Stuff
+import numpy as np
+import CoolProp.CoolProp as Cool
+from CoolProp.HumidAirProp import HAPropsSI
+from CoolProp.CoolProp import PropsSI
+# Set ASHRAE reference to check from WISC.edu
+Cool.set_reference_state('R134a', 'ASHRAE')
+
+# Problem 1
+
